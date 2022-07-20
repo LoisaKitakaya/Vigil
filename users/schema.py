ecf04d454a60a7bc376a93d6eb00787e8802b7a3
Freedom Loisa <kitakayaloisa@gmail.com>
2022-07-20 14:47:33 +0300

    created users app schema

--- users/schema.py
+++ users/schema.py
@@ -64,6 +64,7 @@ class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+    revoke_token = graphql_jwt.Revoke.Field()
 
     # custom mutations
     register_user = UserMutation.Field()
--- vigil_backend/settings.py
+++ vigil_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # force_text is removed from Django 4.0
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
     # 3rd party apps
     'graphene_django',
+    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
 ]
 
 MIDDLEWARE = [
@@ -157,3 +159,10 @@ AUTHENTICATION_BACKENDS = [
     "graphql_jwt.backends.JSONWebTokenBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+GRAPHQL_JWT = {
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    "JWT_EXPIRATION_DELTA": timedelta(days=7),
+    "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=7),
+}
