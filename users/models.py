bf3908d86e49895a00c606201600d01f622935ab
Freedom Loisa <kitakayaloisa@gmail.com>
2022-07-20 08:38:54 +0300

    created custom user model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+
+    email = models.EmailField(blank=False, max_length=254, verbose_name="email address", unique=True)
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
