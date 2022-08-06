e4980e4c61aa44fb3bf5d402dbe78f178678cc60
Freedom Loisa <kitakayaloisa@gmail.com>
2022-08-06 14:29:25 +0300

    added some minor styling to main template and home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,4 @@
 $(document).ready(() => {
-  // active navigation links
-
-  $("#dashboard").click(() => {
-    $("#dashboard").addClass("active");
-    $("#signin").removeClass("active");
-    $("#cart").removeClass("active");
-  });
-
-  $("#signin").click(() => {
-    $("#signin").addClass("active");
-    $("#dashboard").removeClass("active");
-    $("#cart").removeClass("active");
-  });
-
-  $("#cart").click(() => {
-    $("#cart").addClass("active");
-    $("#signin").removeClass("active");
-    $("#dashboard").removeClass("active");
-  });
-
-  // active navigation links
-
   // back to top button
 
   $("#back-top").hide();
@@ -28,7 +6,7 @@ $(document).ready(() => {
   $(window).scroll(function () {
     var scroll = $(document).scrollTop();
 
-    if (scroll > 100) {
+    if (scroll > 300) {
       $("#back-top").fadeIn();
     } else {
       $("#back-top").fadeOut();
