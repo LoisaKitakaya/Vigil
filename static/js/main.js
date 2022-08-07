1cf47cd49ffa0906de48764205fd49a1dded1cde
Freedom Loisa <kitakayaloisa@gmail.com>
2022-08-07 21:06:57 +0300

    finished styling product page and some minor site style changes

--- static/js/main.js
+++ static/js/main.js
@@ -14,4 +14,41 @@ $(document).ready(() => {
   });
 
   // back to top button
+
+  // product details navigation
+
+  $("#write-review").hide();
+  $("#description").hide();
+
+  $("#reviews-nav").click(() => {
+    $("#reviews-nav").addClass("active");
+    $("#description-nav").removeClass("active");
+    $("#write-review-nav").removeClass("active");
+
+    $("#reviews").fadeIn();
+    $("#write-review").hide();
+    $("#description").hide();
+  });
+
+  $("#description-nav").click(() => {
+    $("#reviews-nav").removeClass("active");
+    $("#description-nav").addClass("active");
+    $("#write-review-nav").removeClass("active");
+
+    $("#reviews").hide();
+    $("#write-review").hide();
+    $("#description").fadeIn();
+  });
+
+  $("#write-review-nav").click(() => {
+    $("#reviews-nav").removeClass("active");
+    $("#description-nav").removeClass("active");
+    $("#write-review-nav").addClass("active");
+
+    $("#reviews").hide();
+    $("#write-review").fadeIn();
+    $("#description").hide();
+  });
+
+  // product details navigation
 });
