bbf70d54a070c1860bbf342eda2a26fbcd128b94
Freedom Loisa <kitakayaloisa@gmail.com>
2022-08-08 08:01:55 +0300

    added search functionality but has error

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<slug:slug>/', views.single_product, name="single-product"),
     path('tag/<slug:slug>/', views.tag_filter, name="tag-filter"),
     path('category/<slug:slug>/', views.category_filter, name="category-filter"),
+    path('search/', views.search_filter, name="query-product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
+
+# all products view
 def all_products(request):
 
     products = Product.objects.all()
@@ -12,6 +15,7 @@ def all_products(request):
 
     return render(request, 'products/all_products.html', context)
 
+# single product view
 def single_product(request, slug):
 
     product = Product.objects.get(slug=slug)
@@ -22,22 +26,53 @@ def single_product(request, slug):
 
     return render(request, 'products/product.html', context)
 
+# category filter view
 def category_filter(request, slug):
 
     category = Product.objects.filter(product_category__slug=slug)
 
     context = {
         'this_category': category,
+        'category': slug,
     }
 
     return render(request, 'products/categories.html', context)
 
+# tag filter view
 def tag_filter(request, slug):
 
     tag = Product.objects.filter(product_tags__slug=slug)
 
     context = {
         'this_tag': tag,
+        'tag': slug,
     }
 
     return render(request, 'products/tags.html', context)
+
+# search filter view
+def search_filter(request):
+
+    if request.method == 'GET':
+
+        query = request.GET['search']
+
+        try:
+
+            query_result = Product.objects.filter(
+                Q(product_name__icontains=query) | 
+                Q(full_name__icontains=query) | 
+                Q(product_category__category_name__icontains=query) | 
+                Q(product_tags__tag_name__icontains=query)
+            )
+
+        except query_result.DoesNotExist:
+
+            query_result = None
+
+    context = {
+        'query_results': query_result,
+        'query_name': query, 
+    }
+
+    return render(request, 'products/search_results.html', context)
